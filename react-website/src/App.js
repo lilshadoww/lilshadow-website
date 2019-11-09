3387ba2324264774af851b1a460fde9974ad725c
Naman Avasthi <naman.avasthi@gmail.com>
2019-11-09 02:32:13 -0800

    updates after making private repo public

---
+++ react-website/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CRA : Coming Soon!</h1>
+    </div>
+  );
+}
+
+export default App;
